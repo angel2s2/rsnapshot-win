f88835436482d99c4130e2949772f87ac632d72b
angel2s2 <angel2s2ru@gmail.com>
2025-12-29 14:46:19 +0200

    Update rsnapshot-win.bat
    Переписал скрипт на ln.exe, вместо robocopy, т.к. робокоп не создает новые файлы, а которывает их на запись и заменяет содержимое из-за чего не разрываются жесткие ссылки и файлы в пред. копиях становятся такими же, как в последней копии.

--- rsnapshot-win.bat
+++ rsnapshot-win.bat
@@ -1,124 +1,101 @@
 :: github https://github.com/angel2s2/rsnapshot-win
 
 @echo off
-chcp 1251 >nul
-setlocal ENABLEDELAYEDEXPANSION
 
-:: Источник
-set "SRC=\\10.10.10.10\share"
-:: Хранилище
-set "DSTROOT=C:\bak"
-:: Количества сохраняемых копий (обязательно >0)
-set "MAX_COPIES=10"
+REM
+REM Check for commandline args
+REM
+if "[%~1]" == "[]" goto error
+if "[%~2]" == "[]" goto error
 
+set "LN=D:\tmp\rsnapshot-win-test\ln.exe"
 
+if not exist "%~1" goto error
+if not exist "%~2" goto error
 
-:: Получение даты в формате ГГГГ-ММ-ДД_ЧЧ-ММ
-for /f "tokens=1-3 delims=." %%a in ('date /t') do (
-    set "DD=%%a"
-    set "MM=%%b"
-    set "YYYY=%%c"
-)
-for /f "tokens=1-2 delims=:" %%a in ('time /t') do (
-    set "HOUR=%%a"
-    set "MIN=%%b"
-)
-:: Удаляем пробелы из даты и времени
-set "DD=!DD: =!"
-set "MM=!MM: =!"
-set "YYYY=!YYYY: =!"
-set "HOUR=!HOUR: =0!"
-set "MIN=!MIN: =0!"
+setLocal EnableDelayedExpansion
 
-:: Формируем путь хранения
-set "BACKUP_DATE=!YYYY!-!MM!-!DD!_!HOUR!-!MIN!"
-set "DST=!DSTROOT!\!BACKUP_DATE!"
+REM
+REM Get date and time for timestamps on directories
+REM
+for /f "delims=" %%a in ('%LN% --datetime') do set DATETIMESTAMP=%%a
 
-:: Создать корневую папку, если не существует
-if not exist "!DSTROOT!\" mkdir "!DSTROOT!"
+set SourceDir=%~n1%~x1
+set DestDir=%~2
 
-
-
-:: 1. Находим предыдущую папку-версию
-set "PREV="
-for /f "delims=" %%L in ('dir /b /ad /o-d "!DSTROOT!" 2^>nul') do (
-    set "PREV=!DSTROOT!\%%L"
-    goto :create_hardlinks
-)
-
-
-:create_hardlinks
-:: Создаем папку назначения
-if not exist "!DST!\" mkdir "!DST!"
-
-:: Проверка источника
-if not exist "!SRC!\" (
-    echo ERROR: Source not available >> "!DST!\log_!BACKUP_DATE!.log"
-    exit /b 1
-)
-
-echo Backup start: !DST! > "!DST!\log_!BACKUP_DATE!.log"
-
-
-
-:: 2. Если есть предыдущая версия — создаем жесткие ссылки на все файлы
-if not "!PREV!"=="" (
-    echo Creating hardlinks from previous backup: !PREV! >> "!DST!\log_!BACKUP_DATE!.log"
-    set START_TIME=!time!
-    
-    :: Рекурсивно создаем жесткие ссылки на все файлы из предыдущего бэкапа
-    for /f "delims=" %%F in ('dir "!PREV!" /s /b /a-d 2^>nul ^| findstr /v /i "\\log_"') do (
-        :: Получаем относительный путь файла
-        set "fullPath=%%~F"
-        set "relPath=!fullPath:%PREV%=!"
-		set "relPath=!relPath:~1!"
-		
-		:: Получаем путь к папке (без имени файла)
-		set "targetDir=!DST!\!relPath!"
-		set "targetDir=!targetDir:\%%~nxF=!"
-		if not exist "!targetDir!\" mkdir "!targetDir!"
-		
-        :: Создаем жесткую ссылку в новой папке
-        fsutil hardlink create "!DST!\!relPath!" "%%F" >nul 2>&1 || (
-			echo WARNING: Failed to create hardlink for !relPath! >> "!DST!\log_!BACKUP_DATE!.log"
-		)
-    )
-	set END_TIME=!time!
-    echo Hardlinks created in !START_TIME! - !END_TIME! >> "!DST!\log_!BACKUP_DATE!.log"
+REM Take care of root source folder, e.g. c:\
+if "%SourceDir%"=="" (
+  set SourceDir=%~d1
+  set SourceDir=!SourceDir:~0,1!
+  set SourcePath=%~d1\\
+  REM set BACKUPOPTIONS=--excludedir "System Volume Information" --exclude "~$*" --exclude "Thumbs.db" --exclude "log_*.log"
 ) else (
-    echo No previous backup found, creating first full copy... >> "!DST!\log_!BACKUP_DATE!.log"
+  set SourcePath=%~1
 )
 
+REM
+REM Do the Delorean Copy
+REM
+pushd %DestDir%
+set errlev=0
+set "BACKUPCREATED=%DestDir%\%DATETIMESTAMP%"
+set "BACKUPLOG=%BACKUPCREATED%\log_%DATETIMESTAMP%.log"
+set BACKUPOPTIONS=--quiet 3 --excludedir "System Volume Information" --exclude "~$*" --exclude "Thumbs.db" --exclude "log_*.log"
 
 
-:: 3. Копируем только изменённые файлы
-echo Starting copy... >> "!DST!\log_!BACKUP_DATE!.log"
-robocopy "!SRC!" "!DST!" /e /z /copy:DAT /XO /XF ~$* Thumbs.db /r:1 /w:1 /mt:8 /np /log+:"!DST!\log_!BACKUP_DATE!.log"
-
-:: Проверка успешности копирования
-set /a ROBORESULT=!ERRORLEVEL!
-echo Robocopy exit code: !ROBORESULT! >> "!DST!\log_!BACKUP_DATE!.log"
-:: Коды 0-7 - нормально, продолжаем
-:: Коды 8+ - фатальная ошибка
-if !ROBORESULT! EQU 0 (
-    echo Perfect copy - no errors >> "!DST!\log_!BACKUP_DATE!.log"
-) else if !ROBORESULT! LEQ 7 (
-    echo Copy completed with warnings >> "!DST!\log_!BACKUP_DATE!.log"
+if exist "????-??-?? ??-??-??" (
+  for /f "delims=" %%a in ('dir /b /AD /O:N "????-??-?? ??-??-??"') do set LastBackup=%%a
+  popd
+  mkdir "%BACKUPCREATED%"
+  type nul > "%BACKUPLOG%"
+  REM echo delorean >> "%BACKUPLOG%" 2>&1
+  REM echo "%SourcePath%" >> "%BACKUPLOG%" 2>&1
+  REM echo "%DestDir%\!LastBackup!" >> "%BACKUPLOG%" 2>&1
+  REM echo "%BACKUPCREATED%" >> "%BACKUPLOG%" 2>&1
+  %LN% %BACKUPOPTIONS% --delorean "%SourcePath%" "%DestDir%\!LastBackup!" "%BACKUPCREATED%" >> "%BACKUPLOG%" 2>&1
+  set errlev=!errorlevel!
 ) else (
-    echo ERROR: Fatal robocopy error !ROBORESULT! >> "!DST!\log_!BACKUP_DATE!.log"
-    echo Cleaning up failed backup... >> "!DST!\log_!BACKUP_DATE!.log"
-    rmdir /s /q "!DST!" 2>nul
-    exit /b !ROBORESULT!
+  popd
+  mkdir "%BACKUPCREATED%"
+  type nul > "%BACKUPLOG%"
+  REM echo copy >> "%BACKUPLOG%" 2>&1
+  REM echo "%SourcePath%" >> "%BACKUPLOG%" 2>&1
+  REM echo "%DestDir%\%DATETIMESTAMP%" >> "%BACKUPLOG%" 2>&1
+  %LN% %BACKUPOPTIONS% --copy "%SourcePath%" "%DestDir%\%DATETIMESTAMP%" >> "%BACKUPLOG%" 2>&1
+  set errlev=!errorlevel!
 )
+if %errlev% NEQ 0 goto errorexit
 
-
-:: 4. Очистка старых копий (оставляем последние !MAX_COPIES!)
-set /a SKIP_COUNT=!MAX_COPIES!
-for /f "skip=%SKIP_COUNT% delims=" %%I in ('dir "!DSTROOT!" /ad /b /od 2^>nul') do (
-        echo Deleting old backup: %%I >> "!DST!\log_!BACKUP_DATE!.log"
-        rmdir /s /q "!DSTROOT!\%%I" || echo Could not delete: %%I
+REM
+REM Remove old backup sets (if KeepMaxCopies has been provided)
+REM
+if not "%3"=="" (set /a KeepMax=%~3) else (set KeepMax=0)
+pushd %DestDir%
+if %KeepMax% GTR 0 (
+  echo ................................................... >> "%BACKUPLOG%" 2>&1
+  for /f "skip=%KeepMax% tokens=* delims=" %%G in ('dir /b /A:D /O:-N "????-??-?? ??-??-??"') do (
+    echo Removing backup set "%%G"... >> "%BACKUPLOG%" 2>&1
+    %LN% --quiet 0 --delete "%%G" 2>>"%BACKUPLOG%" 1>nul && echo ...done >> "%BACKUPLOG%" 2>&1 || echo ...FAIL >> "%BACKUPLOG%" 2>&1
+  )
 )
+popd
 
+goto :EOF
 
-echo Backup completed: !DST! >> "!DST!\log_!BACKUP_DATE!.log"
-exit /b 0
+REM
+REM Usage
+REM
+:error
+echo Usage: %~n0 ^<SourcePath^> ^<DestPath^> (^<KeepMaxCopies^>)
+echo.
+echo  SourcePath    directory containing the files to be backed up
+echo  DestPath      directory where "DeLorean copy" sets will be created
+echo  KeepMaxCopies (opt.) remove any exceeding number of DLC sets before copying
+echo.
+echo e.g. %~n0 c:\data\source c:\data\backup 90
+goto :EOF
+
+:errorexit
+echo ................................................... >> "%BACKUPLOG%" 2>&1
+echo Error: %errlev% >> "%BACKUPLOG%" 2>&1
+exit /b %errlev%
